1f129604c940001d58e0559af26a15cc9120596b
Miguel Ángel Durán <miduga@gmail.com>
2021-09-18 16:42:22 +0200

    feat(Root): Start project of TikTok clone

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <main>
+        TikTok Clone
+      </main>
+    </div>
+  )
+}
+
+export default App

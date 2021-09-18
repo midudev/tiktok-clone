e43832d49b356c966870b1b9b77f6e8ffddbefb6
Miguel Ángel Durán <miduga@gmail.com>
2021-09-18 19:14:27 +0200

    feat(Root): Add handlers for Actions on Video

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -5,23 +5,34 @@ import { Comment } from '../Icons/Comment.jsx'
 import styles from './styles.module.css'
 
 export default function VideoPlayerActions ({ likes = 2041, comments = 333, shares = 520, hearted = false }) {
+  const handleLike = () => {
+    window.alert('like')
+  }
+
+  const handleComment = () => {
+    window.alert('comment')
+  }
+
+  const handleShare = () => {
+    window.alert('share')
+  }
+
   return (
     <aside className={styles.actions}>
-      <div className={styles.action}>
-        <Heart />
-        <strong title='like'>{likes}</strong>
-      </div>
+      <button onClick={handleLike} className={styles.action}>
+        <Heart width='45' />
+        <span title='like'>{likes}</span>
+      </button>
 
-      <div className={styles.action}>
-        <Comment />
-        <strong title='comments'>{comments}</strong>
-      </div>
+      <button onClick={handleComment} className={styles.action}>
+        <Comment width='45' />
+        <span title='comments'>{comments}</span>
+      </button>
 
-      <div className={styles.action}>
-        <Share />
-        <strong title='shares'>{shares}</strong>
-      </div>
-
+      <button onClick={handleShare} className={styles.action}>
+        <Share width='45' />
+        <span title='shares'>{shares}</span>
+      </button>
     </aside>
   )
 }

c7c9c44e214ce643b778ff80b1a02b6f821b38e0
Miguel Ángel Durán <miduga@gmail.com>
2021-09-18 17:45:00 +0200

    feat(Root): Use FeedVideos component to show more than one video

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
 import './App.css'
-import VideoPlayer from './components/VideoPlayer/index.jsx'
+import FeedVideos from './components/FeedVideos/index.jsx'
 
 function App () {
   return (
     <div className='App'>
       <main>
-        <VideoPlayer />
+        <FeedVideos />
       </main>
     </div>
   )

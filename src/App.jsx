8549e497249a975371590e86cb1e94dceb1a17db
Miguel Ángel Durán <miduga@gmail.com>
2021-09-19 00:33:19 +0200

    feat: Add new route for upload page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { Route } from 'wouter'
 import FeedVideos from './components/FeedVideos/index.jsx'
+import Upload from './pages/Upload/index.jsx'
 
 function App () {
   return (
     <div className='App'>
       <main>
-        <FeedVideos />
+        <Route path='/'>
+          <FeedVideos />
+        </Route>
+
+        <Route path='/upload'>
+          <Upload />
+        </Route>
       </main>
     </div>
   )

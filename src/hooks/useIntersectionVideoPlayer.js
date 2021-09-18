fcc3a8d555788b1c2fabe67bff47a4bb599d12f4
Miguel Ángel Durán <miduga@gmail.com>
2021-09-18 22:43:52 +0200

    Create new hook to handle video player and intersections

---
+++ src/hooks/useIntersectionVideoPlayer.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+
+const options = {
+  root: document.querySelector('main'),
+  rootMargin: '0px',
+  threshold: 0.9
+}
+
+const observer = new window.IntersectionObserver((entries) => {
+  entries
+    .forEach(entry => {
+      const { target, isIntersecting } = entry
+      target._handleIntersect(isIntersecting)
+    })
+}, options)
+
+const prevHandlePlay = null
+
+export default function useIntersectionVideoPlayer ({ video }) {
+  const [playing, setPlaying] = useState(false)
+
+  console.log('useIntersectionVideoPlayer')
+
+  useEffect(() => {
+    if (!video.current) return
+
+    observer.observe(video.current)
+    video.current._handleIntersect = (isIntersecting) => {
+      const { current: videoEl } = video
+
+      isIntersecting
+        ? videoEl.play()
+        : videoEl.pause()
+
+      setPlaying(!videoEl.paused)
+    }
+  }, [video.current])
+
+  const handlePlay = () => {
+    const { current: videoEl } = video
+    playing
+      ? videoEl.pause()
+      : videoEl.play()
+
+    setPlaying(!playing)
+  }
+
+  return {
+    handlePlay,
+    playing
+  }
+}

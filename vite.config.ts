2216a1f4fcea633e8ba8673738ee1d54084badb2
Miguel Ángel Durán <miduga@gmail.com>
2021-09-18 17:04:50 +0200

    chore(Root): Auto import React JSX

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    jsxFactory: '_jsx',
+    jsxFragment: '_jsxFragment',
+    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`,
+  },
   plugins: [reactRefresh()]
 })

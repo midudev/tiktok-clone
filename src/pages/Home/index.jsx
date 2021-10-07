119ac6dc545c4636a8bc2fdb7e6561169f101012
Miguel Ángel Durán <miduga@gmail.com>
2021-10-07 20:31:35 +0200

    Create new Home page component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import FeedVideos from '../../components/FeedVideos/index.jsx'
+import NavBar from '../../components/NavBar/index.jsx'
+
+export default function Home () {
+  return (
+    <div>
+      <FeedVideos />
+      <NavBar />
+    </div>
+  )
+}

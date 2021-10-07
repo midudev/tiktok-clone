d2d9b631bbbecb9af239c20955dae45b2229fb4c
Miguel Ángel Durán <miduga@gmail.com>
2021-10-07 20:31:42 +0200

    Create new Login page component

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,19 @@
+import { supabase } from '../../services/supabase.js'
+
+export default function Login () {
+  const handleClick = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'twitter'
+    })
+
+    console.log({ user, session, error })
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        Login with Twitter
+      </button>
+    </div>
+  )
+}

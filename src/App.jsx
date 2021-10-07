151021aebd2becd14ceff7a395af8fb9b5b7b693
Miguel Ángel Durán <miduga@gmail.com>
2021-10-07 20:30:57 +0200

    Add Login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import './App.css'
+import { lazy, Suspense } from 'react'
 import { Route } from 'wouter'
-import FeedVideos from './components/FeedVideos/index.jsx'
-import Upload from './pages/Upload/index.jsx'
+
+const Home = lazy(() => import('./pages/Home/index.jsx'))
+const Upload = lazy(() => import('./pages/Upload/index.jsx'))
+const Login = lazy(() => import('./pages/Login/index.jsx'))
 
 function App () {
   return (
     <div className='App'>
       <main>
-        <Route path='/'>
-          <FeedVideos />
-        </Route>
+        <Suspense fallback={<div />}>
+          <Route path='/'>
+            <Home />
+          </Route>
 
-        <Route path='/upload'>
-          <Upload />
-        </Route>
+          <Route path='/upload'>
+            <Upload />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Suspense>
       </main>
     </div>
   )
